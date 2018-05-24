a12c0fccdd137e57a0bc48b9f49223b587d79f63
William Bradshaw <w.a.bradshaw@gmail.com>
2018-05-24 11:57:26 +0100

    Added a save method that takes BufferedImages

--- src/main/java/com/wabradshaw/palettest/utils/ImageFileUtils.java
+++ src/main/java/com/wabradshaw/palettest/utils/ImageFileUtils.java
@@ -1,6 +1,10 @@
 package com.wabradshaw.palettest.utils;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * A class used for simple file operations like saving a copy to disk for an actual review.
@@ -10,18 +14,47 @@ public class ImageFileUtils {
     /**
      * Saves a byte array to the supplied path.
      *
-     * @param output A byte array containing the file contents.
-     * @param path   Where the file should be stored, including the path, name and extension
+     * @param content A byte array containing the file contents.
+     * @param path    Where the file should be stored, including the path, name and extension
      * @throws IllegalArgumentException If the byte array is null.
      * @throws RuntimeException         If the file could not be saved.
      */
-    public static void save(byte[] output, String path) {
-        if (output == null) {
+    public static void save(byte[] content, String path) {
+        if (content == null) {
             throw new IllegalArgumentException("Could not save an empty byte array to " + path + ".");
         } else {
             try {
-                new FileOutputStream(path).write(output);
-            } catch (Exception ex) {
+                new FileOutputStream(path).write(content);
+            } catch (IOException ex) {
+                throw new RuntimeException("Could not save the file " + path + ".", ex);
+            }
+        }
+    }
+
+    /**
+     * Saves a {@link BufferedImage} as the supplied file type to the supplied path.
+     *
+     * Please note that the path should include the desired extension. The path's extension is what the file is saved
+     * under, while the fileType is the extension defining the type of image. These should typically be the same.
+     *
+     * @param content   A byte array containing the file contents.
+     * @param path      Where the file should be stored, including the path, name and extension.
+     * @param fileType  The type of image being processed, e.g. "png" or "jpg".
+     * @throws IllegalArgumentException If the byte array is null.
+     * @throws RuntimeException         If the file could not be saved.
+     */
+    public static void save(BufferedImage content, String path, String fileType) {
+        if (content == null) {
+            throw new IllegalArgumentException("Could not save an empty BufferedImage to " + path + ".");
+        } else {
+            try {
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                ImageIO.write(content, fileType, outputStream);
+                outputStream.flush();
+                byte[] array = outputStream.toByteArray();
+
+                new FileOutputStream(path).write(array);
+            } catch (IOException ex) {
                 throw new RuntimeException("Could not save the file " + path + ".", ex);
             }
         }
--- src/test/java/com/wabradshaw/palettest/utils/ImageFileUtilsTest.java
+++ src/test/java/com/wabradshaw/palettest/utils/ImageFileUtilsTest.java
@@ -27,16 +27,14 @@ public class ImageFileUtilsTest {
      * @throws IOException - If the file with the test image can't be loaded.
      */
     @Test
-    void testSaveFile() throws IOException {
+    void testSaveArray() throws IOException {
         BufferedImage original = ImageIO.read(this.getClass().getResourceAsStream("/sampleImages/maps/Rome.png"));
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ImageIO.write(original, "png", outputStream);
         outputStream.flush();
         byte[] array = outputStream.toByteArray();
 
-        TemporaryFolder tempFolder = new TemporaryFolder();
-        tempFolder.create();
-        File outputLocation = new File(tempFolder.getRoot().getPath() + "/saveArray.png");
+        File outputLocation = createTemporaryFileLocation("/saveArray.png");
 
         assertEquals(false, outputLocation.exists());
         ImageFileUtils.save(array, outputLocation.getPath());
@@ -49,11 +47,66 @@ public class ImageFileUtilsTest {
      * @throws IOException - If the file with the test image can't be loaded.
      */
     @Test
-    void testSaveFile_null() throws IOException {
-        TemporaryFolder tempFolder = new TemporaryFolder();
-        tempFolder.create();
-        File outputLocation = new File(tempFolder.getRoot().getPath() + "/saveNullArray.png");
+    void testSaveArray_null() throws IOException {
+        File outputLocation = createTemporaryFileLocation("/saveNullArray.png");
 
         assertThrows(RuntimeException.class, ()-> ImageFileUtils.save(null, outputLocation.getPath()));
+    }
+
+    /**
+     * Tests the save method when used on a buffered image.
+     *
+     * @throws IOException - If the file with the test image can't be loaded.
+     */
+    @Test
+    void testSaveImage() throws IOException {
+        BufferedImage original = ImageIO.read(this.getClass().getResourceAsStream("/sampleImages/maps/Rome.png"));
+
+        File outputLocation = createTemporaryFileLocation("/saveBufferedImage.png");
+
+        assertEquals(false, outputLocation.exists());
+        ImageFileUtils.save(original, outputLocation.getPath(), "png");
+        assertEquals(true, outputLocation.exists());
+    }
+
+    /**
+     * Tests the save method when used on a buffered image.
+     *
+     * @throws IOException - If the file with the test image can't be loaded.
+     */
+    @Test
+    void testSaveImage_null() throws IOException {
+        File outputLocation = createTemporaryFileLocation("/saveNullImage.png");
+
+        assertThrows(RuntimeException.class, ()-> ImageFileUtils.save(null, outputLocation.getPath(), "png"));
+    }
+    /**
+     * Tests that saving a BufferedImage with a different extension to the path will still work.
+     *
+     * @throws IOException - If the file with the test image can't be loaded.
+     */
+    @Test
+    void testSaveImage_differentExtension() throws IOException {
+        BufferedImage original = ImageIO.read(this.getClass().getResourceAsStream("/sampleImages/maps/Rome.png"));
+
+        File outputLocation = createTemporaryFileLocation("/saveAsJpg.jpg");
+
+        assertEquals(false, outputLocation.exists());
+        ImageFileUtils.save(original, outputLocation.getPath(), "png");
+        assertEquals(true, outputLocation.exists());
+    }
+
+    /**
+     * Creates a File object in a temporary folder. This folder will be cleared up after the test runs. Does not
+     * instantiate the file on disk, only as a Java object.
+     *
+     * @param location The name of the file (includes extension, doesn't need a path
+     * @return A pointer to the file location
+     * @throws IOException If the temporary folder couldn't be created.
+     */
+    private File createTemporaryFileLocation(String location) throws IOException{
+        TemporaryFolder tempFolder = new TemporaryFolder();
+        tempFolder.create();
+        return new File(tempFolder.getRoot().getPath() + location);
     }
 }
