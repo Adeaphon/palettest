c4148dcbe23e7a314476c6efe6ac9029d6c2ec6e
William Bradshaw <w.a.bradshaw@gmail.com>
2018-06-20 15:59:12 +0100

    Class description for the palettester

--- src/main/java/com/wabradshaw/palettest/analysis/Palettester.java
+++ src/main/java/com/wabradshaw/palettest/analysis/Palettester.java
@@ -1,4 +1,28 @@
 package com.wabradshaw.palettest.analysis;
 
+/**
+ * <p>
+ * The main class behind the whole Palettest project. A {@link Palettester} is used to analyse the colors that make up
+ * an image. As it is often inconvenient to test against every single pixel color in an image, the notion of palettes is
+ * used to cluster similar colors together into a general {@link Tone}. For example, #ff0000 and #ff00001 would likely
+ * be part of a single 'red' {@link Tone}. The collection of {@link Tone}s to test against is called a palette.
+ *</p>
+ * <p>
+ * An image is tested using three main methods:
+ * </p>
+ * <ul>
+ * <li>analysePalette(Palette, Image) - Used to count how many times each general {@link Tone} in a pre-defined
+ *                                      palette is used.</li>
+ * <li>analyseColors(Image) - Used to take a count of every single color in an image. This treats every hex code as a
+ *                            separate {@link Tone}.</li>
+ * <li>definePalette(Image, Clusters) - Used to define a new palette from the image, then to count how many times each
+ *                                      {@link Tone} is used. Used when the palette is not known up front.</li>
+ * </ul>
+ */
 public class Palettester {
+
+    public Palettester(){
+        /* no-op */
+    }
+
 }
