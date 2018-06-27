e894459fc9fd57997011966e8897679e0527e8b8
William Bradshaw <w.a.bradshaw@gmail.com>
2018-06-27 13:10:04 +0100

    Euclidean rgba distance added

---
+++ src/main/java/com/wabradshaw/palettest/analysis/distance/EuclideanRgbaDistance.java
@@ -0,0 +1,31 @@
+package com.wabradshaw.palettest.analysis.distance;
+
+import com.wabradshaw.palettest.analysis.Tone;
+
+/**
+ * <p>
+ * A {@link ColorDistanceFunction} that finds the Euclidean distance between two colors in RGBA space. Effectively
+ * this means that the colors are treated as points in 4d space (red, green, blue, alpha), and this measures a
+ * theoretical straight line between them.
+ * </p>
+ * <p>
+ * Please note that because of the nature of transparency, this metric does not work well for very transparent colors.
+ * For example, completely transparent red and completely transparent blue will have a high distance despite being
+ * identical.
+ * </p>
+ */
+public class EuclideanRgbaDistance implements ColorDistanceFunction {
+
+    @Override
+    public double getDistance(Tone first, Tone second) {
+        return Math.sqrt(getRankingDistance(first, second));
+    }
+
+    @Override
+    public double getRankingDistance(Tone first, Tone second) {
+        return Math.pow((first.getRed() - second.getRed()), 2) +
+               Math.pow((first.getGreen() - second.getGreen()), 2) +
+               Math.pow((first.getBlue() - second.getBlue()), 2) +
+               Math.pow((first.getAlpha() - second.getAlpha()), 2);
+    }
+}
