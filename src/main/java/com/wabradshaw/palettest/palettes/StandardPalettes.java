9a6c646517a73e8457c3ae191381aed0883cf363
William Bradshaw <w.a.bradshaw@gmail.com>
2018-07-12 10:40:10 +0100

    Added rainbow and rainbow_bw to the list of standard color palettes.

--- src/main/java/com/wabradshaw/palettest/palettes/StandardPalettes.java
+++ src/main/java/com/wabradshaw/palettest/palettes/StandardPalettes.java
@@ -12,6 +12,8 @@ import java.util.List;
  * </p>
  * <ul>
  * <li>JAVA_COLORS - The standard colors available through the Color class</li>
+ * <li>RAINBOW - The colors of the rainbow.</li>
+ * <li>RAINBOW_BW - The colors of the rainbow, as well as black and white.</li>
  * </ul>
  */
 public class StandardPalettes {
@@ -32,4 +34,30 @@ public class StandardPalettes {
                                                                new Tone("Dark Gray", Color.darkGray),
                                                                new Tone("Orange", Color.orange),
                                                                new Tone("Pink", Color.pink));
+
+    /**
+     * A palette containing the colors of the rainbow. Color codes found on Webnots.com
+     * @see <a href="https://www.webnots.com/vibgyor-rainbow-color-codes/">Color codes</a>
+     */
+    public static final List<Tone> RAINBOW = Arrays.asList(new Tone("Violet", new Color(148,0,211)),
+                                                           new Tone("Indigo", new Color(75,0,130)),
+                                                           new Tone("Blue", new Color(0,0,255)),
+                                                           new Tone("Green", new Color(0,255,0)),
+                                                           new Tone("Yellow", new Color(255,255,0)),
+                                                           new Tone("Orange", new Color(255,127,0)),
+                                                           new Tone("Red", new Color(255,0,0)));
+
+    /**
+     * A palette containing the colors of the rainbow, as well as black and white. Color codes found on Webnots.com
+     * @see <a href="https://www.webnots.com/vibgyor-rainbow-color-codes/">Color codes</a>
+     */
+    public static final List<Tone> RAINBOW_BW = Arrays.asList(new Tone("Violet", new Color(148,0,211)),
+                                                              new Tone("Indigo", new Color(75,0,130)),
+                                                              new Tone("Blue", new Color(0,0,255)),
+                                                              new Tone("Green", new Color(0,255,0)),
+                                                              new Tone("Yellow", new Color(255,255,0)),
+                                                              new Tone("Orange", new Color(255,127,0)),
+                                                              new Tone("Red", new Color(255,0,0)),
+                                                              new Tone("Black", new Color(0,0,0)),
+                                                              new Tone("White", new Color(255,255,255)));
 }
