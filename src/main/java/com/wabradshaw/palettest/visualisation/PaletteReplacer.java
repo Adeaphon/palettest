1ab20fddc4a9200d4a9e1f2b9f95b03c5cce5240
William Bradshaw <w.a.bradshaw@gmail.com>
2018-07-21 18:48:37 +0200

    Added the code for the PaletteReplacer

--- src/main/java/com/wabradshaw/palettest/visualisation/PaletteReplacer.java
+++ src/main/java/com/wabradshaw/palettest/visualisation/PaletteReplacer.java
@@ -1,7 +1,11 @@
 package com.wabradshaw.palettest.visualisation;
 
 import com.wabradshaw.palettest.analysis.Tone;
+import com.wabradshaw.palettest.analysis.distance.ColorDistanceFunction;
+import com.wabradshaw.palettest.analysis.distance.EuclideanRgbaDistance;
+import com.wabradshaw.palettest.utils.GraphicsUtils;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.List;
 
@@ -11,6 +15,15 @@ import java.util.List;
  */
 public class PaletteReplacer {
 
+    private final ColorDistanceFunction distanceFunction;
+
+    /**
+     * Default constructor. Sets up a {@link PaletteReplacer} using {@link EuclideanRgbaDistance} to measure distance.
+     */
+    public PaletteReplacer(){
+        this.distanceFunction = new EuclideanRgbaDistance();
+    }
+
     /**
      * Takes in an image and creates a new version where each pixel has been mapped to the nearest color in the palette.
      *
@@ -19,8 +32,40 @@ public class PaletteReplacer {
      * @return        A version of the input image with each pixel redrawn according to the palette.
      */
     public BufferedImage replace(BufferedImage image, List<Tone> palette) {
+        image = GraphicsUtils.createCopy(image);
+
         BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
 
+        Graphics2D g = result.createGraphics();
+
+        int currentX = 0;
+        int currentY = 0;
+
+        for(int x = 0; x < image.getWidth(); x++){
+            for(int y = 0; y < image.getHeight(); y++){
+                Color original = new Color(image.getRGB(x, y));
+                Tone target = getClosestTone(palette, new Tone("", original));
+                g.setPaint(target.getColor());
+                g.drawLine(x, y, x+1, y+1);
+            }
+        }
+
+        g.dispose();
+
         return result;
+    }
+
+
+    /**
+     * Finds the {@link Tone} closest to the target color.
+     *
+     * @param palette The list of possible Tones in the palette.
+     * @return        The closest Tone in the palette.
+     */
+    private Tone getClosestTone(List<Tone> palette, Tone targetTone) {
+        return palette.stream()
+                .max((o1, o2) -> (int) Math.signum(distanceFunction.getDistance(o2, targetTone) -
+                                                   distanceFunction.getDistance(o1, targetTone)))
+                .get();
     }
 }
--- src/test/java/com/wabradshaw/palettest/visualisation/PaletteReplacerTest.java
+++ src/test/java/com/wabradshaw/palettest/visualisation/PaletteReplacerTest.java
@@ -1,5 +1,7 @@
 package com.wabradshaw.palettest.visualisation;
 
+import com.wabradshaw.palettest.analysis.PaletteDistribution;
+import com.wabradshaw.palettest.analysis.Palettester;
 import com.wabradshaw.palettest.analysis.Tone;
 import com.wabradshaw.palettest.palettes.StandardPalettes;
 import com.wabradshaw.palettest.utils.ImageFileUtils;
@@ -12,8 +14,7 @@ import java.util.List;
 
 import static com.wabradshaw.palettest.assertions.AssertDimensions.assertDimensions;
 import static com.wabradshaw.palettest.assertions.AssertPixelsMatch.assertPixelsMatch;
-import static com.wabradshaw.palettest.utils.ImageFileUtils.save;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * A set of tests for the {@link PaletteVisualiser} class.
@@ -34,5 +35,64 @@ public class PaletteReplacerTest {
         BufferedImage result = replacer.replace(image, palette);
 
         assertDimensions(result, 2, 4);
+    }
+
+    /**
+     * Tests that replacing an image with a single color returns an image with a single color.
+     */
+    @Test
+    public void testSingleColor(){
+        List<Tone> palette = Arrays.asList(new Tone(Color.red));
+
+        BufferedImage image = ImageFileUtils.loadImageResource("/sampleImages/dimensions/2x4.png");
+
+        PaletteReplacer replacer = new PaletteReplacer();
+
+        BufferedImage result = replacer.replace(image, palette);
+
+        PaletteDistribution analysis = new Palettester().analyseAllColors(result);
+
+        assertEquals(1, analysis.byCount().size());
+        assertEquals(palette.get(0), analysis.byCount().get(0).getTone());
+    }
+
+    /**
+     * Tests replacing an image with two colors, using two similar but not identical colors.
+     */
+    @Test
+    public void testTrivialReplacement(){
+        List<Tone> palette = Arrays.asList(new Tone(new Color(200,10,10)),
+                                           new Tone(new Color(10,200,10)),
+                                           new Tone(new Color(10,10,200)));
+
+        BufferedImage image = ImageFileUtils.loadImageResource("/sampleImages/geometric/redBlueQuarter.png");
+
+        PaletteReplacer replacer = new PaletteReplacer();
+
+        BufferedImage result = replacer.replace(image, palette);
+
+        PaletteDistribution analysis = new Palettester().analyseAllColors(result);
+
+        assertEquals(2, analysis.byCount().size());
+        assertEquals(palette.get(0), analysis.byCount().get(0).getTone());
+        assertEquals(palette.get(2), analysis.byCount().get(1).getTone());
+    }
+
+    /**
+     * Tests replacing a complex image.
+     */
+    @Test
+    public void testComplex(){
+        List<Tone> palette = StandardPalettes.PWG_STANDARD;
+
+        BufferedImage image = ImageFileUtils.loadImageResource("/sampleImages/complex/rooves.jpg");
+
+        PaletteReplacer replacer = new PaletteReplacer();
+
+        BufferedImage result = replacer.replace(image, palette);
+
+        BufferedImage target = ImageFileUtils.loadImageResource("/resultImages/paletteReplacer/pwgRooves.png");
+
+        assertPixelsMatch(target, result);
     }
 }
