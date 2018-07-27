30caa7a27612eaac47cd9cccddc123f619226248
William Bradshaw <w.a.bradshaw@gmail.com>
2018-07-27 17:12:04 +0200

    Added an assertMostly method to use to explain the situation where most of an image is part of a single tone.

---
+++ src/main/java/com/wabradshaw/palettest/assertions/AssertMostly.java
@@ -0,0 +1,62 @@
+package com.wabradshaw.palettest.assertions;
+
+import com.wabradshaw.palettest.analysis.PaletteDistribution;
+import com.wabradshaw.palettest.analysis.ToneCount;
+
+import java.util.stream.Collectors;
+
+import static com.wabradshaw.palettest.assertions.AssertionHelpers.fail;
+
+/**
+ * Assertion methods that check that the majority (above 50% of pixels) are part of a certain
+ * {@link com.wabradshaw.palettest.analysis.Tone}.
+ */
+public class AssertMostly {
+
+    /**
+     * <p>
+     * Asserts that the more than 50% of the {@link PaletteDistribution} is the desired color. This finds the color
+     * using its name. Please note that this is strictly more than 50%, so an image which is half one color will fail.
+     * </p>
+     * <p>
+     * This assertion will fail if the distribution is null, or if there are no {@link ToneCount}s in the distribution.
+     * </p>
+     * @param target       The name of the color which should be the majority of the image.
+     * @param distribution The {@link PaletteDistribution} describing an image.
+     */
+    public static void assertMostly(String target, PaletteDistribution distribution){
+        if(distribution == null){
+            fail("Could not assess color as the supplied distribution was null.");
+        } else if(distribution.byCount().isEmpty()){
+            fail("Could not assess color as the supplied distribution didn't contain any colors.");
+        }
+
+        ToneCount count = distribution.get(target);
+
+        int totalPixels = distribution.getDistribution().stream().collect(Collectors.summingInt(ToneCount::getCount));
+        int targetPixels = totalPixels > 1 ? totalPixels / 2 + 1 : 1;
+
+
+        if(count == null){
+            fail("The image didn't contain the desired color.",
+                 pixels(target, targetPixels),
+                 distribution.byCount().toString());
+        } else if (count.getCount() < targetPixels){
+            fail("Not enough of the image was the desired color.",
+                 pixels(target, targetPixels),
+                 distribution.byCount().toString());
+        }
+    }
+
+    /**
+     * A helper method to say pixel or pixels based on whether or not the number is greater than 1.
+     *
+     * @param descriptor A descriptor to describe what type of pixels.
+     * @param pixels     The number of pixels to mention.
+     * @return           A string saying the number of pixels
+     */
+    private static String pixels(String descriptor, int pixels){
+        String pixelsMessage = pixels > 1 ? "pixels" : "pixel";
+        return pixels + " " + descriptor + " " + pixelsMessage;
+    }
+}
---
+++ src/test/java/com/wabradshaw/palettest/assertions/AssertMostlyTest.java
@@ -0,0 +1,153 @@
+package com.wabradshaw.palettest.assertions;
+
+import com.wabradshaw.palettest.analysis.PaletteDistribution;
+import com.wabradshaw.palettest.analysis.Tone;
+import com.wabradshaw.palettest.analysis.ToneCount;
+import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+import static com.wabradshaw.palettest.assertions.AssertMostly.assertMostly;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * A set of tests for {@link AssertDimensions}
+ */
+public class AssertMostlyTest {
+
+    /**
+     * Tests that calling assertMostly using a string will throw an exception if the distribution is null.
+     */
+    @Test
+    public void testByString_NullDistribution(){
+        try {
+            assertMostly("Green", null);
+            fail("Should have thrown an exception.");
+        } catch (AssertionFailedError e){
+            assertEquals(e.getMessage(), "Could not assess color as the supplied distribution was null.");
+        }
+    }
+
+    /**
+     * Tests that calling assertMostly using a string will throw an exception if the distribution doesn't have any
+     * colors in it.
+     */
+    @Test
+    public void testByString_EmptyDistribution(){
+        try {
+            assertMostly("Green", new PaletteDistribution(new ArrayList<>()));
+            fail("Should have thrown an exception.");
+        } catch (AssertionFailedError e){
+            assertEquals(e.getMessage(), "Could not assess color as the supplied distribution didn't contain any colors.");
+        }
+    }
+
+    /**
+     * Tests that calling assertMostly using a string will fail if that color wasn't contained in the distribution.
+     */
+    @Test
+    public void testByString_NotContained(){
+        try {
+            List<ToneCount> counts = Arrays.asList(toneCount(1, "Red", Color.RED));
+            assertMostly("Green", new PaletteDistribution(counts));
+            fail("Should have thrown an exception.");
+        } catch (AssertionFailedError e){
+            assertEquals(e.getExpected().getValue(), "1 Green pixel");
+            assertEquals(e.getActual().getValue(), "[Red: 1]");
+            assertEquals(e.getMessage(), "The image didn't contain the desired color.");
+        }
+    }
+
+    /**
+     * Tests that calling assertMostly using a string will succeed on an image with a single pixel of the desired color.
+     */
+    @Test
+    public void testByString_OnlyColor_OnePixel(){
+        List<ToneCount> counts = Arrays.asList(toneCount(1, "Green", Color.RED));
+        assertMostly("Green", new PaletteDistribution(counts));
+    }
+
+    /**
+     * Tests that calling assertMostly using a string will succeed on an image with several pixel of the desired color.
+     */
+    @Test
+    public void testByString_OnlyColor_ManyPixels(){
+        List<ToneCount> counts = Arrays.asList(toneCount(10, "Green", Color.RED));
+        assertMostly("Green", new PaletteDistribution(counts));
+    }
+
+    /**
+     * Tests that calling assertMostly using a string will fail on an image where the target color is not the main one.
+     */
+    @Test
+    public void testByString_MultipleColors_NotTheMajority(){
+        try {
+            List<ToneCount> counts = Arrays.asList(toneCount(10, "Red", Color.RED),
+                                                   toneCount(1, "Green", Color.GREEN));
+
+            assertMostly("Green", new PaletteDistribution(counts));
+            fail("Should have thrown an exception.");
+        } catch (AssertionFailedError e){
+            assertEquals(e.getExpected().getValue(), "6 Green pixels");
+            assertEquals(e.getActual().getValue(), "[Red: 10, Green: 1]");
+            assertEquals(e.getMessage(), "Not enough of the image was the desired color.");
+        }
+    }
+
+    /**
+     * Tests that calling assertMostly using a string will fail on an image where the target color is not the main one.
+     */
+    @Test
+    public void testByString_MultipleColors_Half(){
+        try {
+            List<ToneCount> counts = Arrays.asList(toneCount(5, "Red", Color.RED),
+                                                   toneCount(5, "Green", Color.GREEN));
+
+            assertMostly("Green", new PaletteDistribution(counts));
+            fail("Should have thrown an exception.");
+        } catch (AssertionFailedError e){
+            assertEquals(e.getExpected().getValue(), "6 Green pixels");
+            assertEquals(e.getActual().getValue(), "[Red: 5, Green: 5]");
+            assertEquals(e.getMessage(), "Not enough of the image was the desired color.");
+        }
+    }
+
+    /**
+     * Tests that calling assertMostly using a string will pass if the target color is only just the majority.
+     */
+    @Test
+    public void testByString_MultipleColors_ExactMajority(){
+        List<ToneCount> counts = Arrays.asList(toneCount(5, "Red", Color.RED),
+                                               toneCount(6, "Green", Color.GREEN));
+
+        assertMostly("Green", new PaletteDistribution(counts));
+    }
+
+    /**
+     * Tests that calling assertMostly using a string will pass if the target color is definitely the majority.
+     */
+    @Test
+    public void testByString_MultipleColors_SevereMajority(){
+        List<ToneCount> counts = Arrays.asList(toneCount(1, "Red", Color.RED),
+                toneCount(6, "Green", Color.GREEN));
+
+        assertMostly("Green", new PaletteDistribution(counts));
+    }
+
+    /**
+     * A helper method to instantiate a {@link ToneCount} that only contains a single color.
+     *
+     * @param pixels The number of pixels in that color.
+     * @param name   The name of the {@link Tone}.
+     * @param color  The {@link Color} the {@link Tone} represents.
+     * @return       A {@link ToneCount} with the specified details.
+     */
+    private ToneCount toneCount(int pixels, String name, Color color){
+        Map<Color, Integer> pixelCounts = new HashMap<>();
+        pixelCounts.put(color, pixels);
+        return new ToneCount(new Tone(name, color), pixelCounts);
+    }
+}
